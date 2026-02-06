4a7e3f21d13fb4eaa37bec076b74e7056463b24e
David Hubbard <Dhubbard5@stu.jsu.edu>
2026-02-06 10:20:14 -0600

    Completed Layout!

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,6 +15,5 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
     }
 }
